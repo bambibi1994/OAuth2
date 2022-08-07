1906fe2fb360427e0994063ec2745568acd3e650
unknown <Bambibi_1994@yahoo.com.vn>
2022-08-07 12:58:43 -0500

    first commit

---
+++ src/test/java/com/tts/Spring/AuthO2/SpringAuthO2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.Spring.AuthO2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthO2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
